cee92f6fc1f1a36689cef13a391d6a502e21c021
Reghis Villarroel Roca <reghisbot@gmail.com>
2023-03-08 17:12:54 -0300

    Un pequeño cambio

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -100,7 +100,7 @@ namespace ConsoleApp2
                     default:
                         break;
                 }
-                Console.ReadKey();
+                //Console.ReadKey();
             }
             
         }
